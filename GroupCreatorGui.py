fe044ae45c4932b832457bc98451ebc8572d374c
Heather MacIsaac <hmacisaa@uwo.ca>
2022-03-02 10:43:23 -0400

    Not able to retrieve grade and num_groups to use in button

--- GroupCreatorGui.py
+++ GroupCreatorGui.py
@@ -12,19 +12,20 @@ question_num_groups= tk.Label(text="How many groups would you like to make?")
 num_groups_entry=tk.Entry()
 num_groups=num_groups_entry.get()
 
-button = tk.Button(
-    text="Determine Groups",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="black",
-)
+
 greeting.pack()
 
 question_grade.pack()
 grade_entry.pack()
 question_num_groups.pack()
 num_groups_entry.pack()
+button = tk.Button(
+    text="Determine Groups",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="black",
+command=Team_Chooser(grade,num_groups))
 button.pack()
 window.mainloop()
 
